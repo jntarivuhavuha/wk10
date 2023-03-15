87358b16f42cd1d4e59fc78449ae47dae7f94fe1
jntarivuhavuha <114191852+jntarivuhavuha@users.noreply.github.com>
2023-03-15 15:15:40 +1100

    Add files via upload

---
+++ API/weatherapi.js
@@ -0,0 +1,75 @@
+const apiURL = 
+ "//api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=2aae4e33100f691521d10a59b4d392f0";
+
+ fetch(apiURL)
+ .then((response) => response.json())
+ .then((jsonObject) => {
+   console.log(jsonObject);
+   document.getElementById("current-temp").textContent = jsonObject.main.temp;
+
+   const imagesrc =
+   "//openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+ const desc = jsonObject.weather[0].description;
+ document.getElementById("imagesrc").textContent = imagesrc;
+ document.getElementById("icon").setAttribute("src", imagesrc);
+ document.getElementById("icon").setAttribute("alt", desc);
+});
+
+
+
+   
+
+
+
+
+
+   
+
+
+   
+
+
+
+   
+
+   
+
+
+
+   
+
+
+   
+  
+
+
+
+   
+
+
+
+
+
+   
+
+
+   
+
+
+
+   
+
+   
+
+
+
+   
+
+
+   
+
+
+
+
+   
+
